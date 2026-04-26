f5ab0499d6e3a053fbfaebb1ed54841f1ee3157f
bo64e <108142631+bo64e@users.noreply.github.com>
2026-04-26 18:41:45 +0100

    s17, minor cleanup

--- static/src/a.py
+++
@@ -1,9 +0,0 @@
-import os,json
-season = 0
-for root, dirs, files in os.walk("C:\\Users\\jonop\\Downloads\\iasip\\static\\src\\sns"):
-    for file in files:
-        if file.endswith(".html"):
-            with open(os.path.join(root,file),"r") as f:
-                a = f.read().splitlines()
-                if "" in [x.strip() for x in a]:
-                    print(file)
